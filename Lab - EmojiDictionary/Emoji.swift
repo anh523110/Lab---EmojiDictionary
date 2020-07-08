a44bd587c52d43d56e0966957d41986bf66d1147
ducanh <toducanh2@gmail.com>
2020-07-08 08:47:45 +0700

    Initial Commit

---
+++ Lab - EmojiDictionary/Emoji.swift
@@ -0,0 +1,23 @@
+//
+//  Emoji.swift
+//  Lab - EmojiDictionary
+//
+//  Created by BVU on 7/1/20.
+//  Copyright © 2020 BVU. All rights reserved.
+//
+
+import Foundation
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+
+    init(symbol: String, name: String, description: String,
+    usage: String) {
+        self.symbol = symbol
+        self.name = name
+        self.description = description
+        self.usage = usage
+    }
+}
